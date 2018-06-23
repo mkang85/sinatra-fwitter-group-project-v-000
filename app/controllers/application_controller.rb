e91a169c94d74ceb46e13c1d9612263bc37164f7
Michael Kang <michaelkang85@gmail.com>
2018-06-23 00:43:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,22 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :'/users/homepage'
+  end
+
+  get '/signup' do
+
+    erb :'/users/signup'
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to :'/signup'
+    end
+      @user = User.create(params)
+    redirect to :'/tweets'
+  end
+
+
 end
